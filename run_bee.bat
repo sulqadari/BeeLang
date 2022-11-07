2f3dc0d92cd06dc6faff39e83a786f1a742dd214
tislamov <telman-islamov@yandex.ru>
2022-11-07 14:11:55 +0300

    minor changes

--- run_bee.bat
+++ run_bee.bat
@@ -4,12 +4,12 @@ chcp 1251
 set src=%~dp0scripts\
 
 rem set input=%src%print_test.bee;
-rem set input=%src%native_clock_test.bee;%input%
+set input=%src%native_clock_test.bee;%input%
 rem set input=%src%equality_test.bee;%input%
 rem set input=%src%environments_test.bee;%input%
 rem set input=%src%parentheses_test.bee;%input%
 rem set input=%src%functions_call_test.bee;%input%
-set input=%src%increment_test.bee;%input%
+rem set input=%src%increment_test.bee;%input%
 
 java -cp bin ru.beelang.Main %input%
 pause
